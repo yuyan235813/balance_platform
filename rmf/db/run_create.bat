be7c929b7ecb29ae8f11960b723fe05f5479394f
李昱岩 <794339312@qq.com>
2018-11-20 23:03:57 +0800

    备份数据库

--- rmf/db/run_create.bat
+++ rmf/db/run_create.bat
@@ -1,5 +1,8 @@
 @echo off
 cd /d %~dp0
+set YYYYmmdd=%date:~0,4%%date:~5,2%%date:~8,2%
+set hhmiss=%time:~0,2%%time:~3,2%%time:~6,2%
+sqlite3 balance.db ".dump" > balance_%YYYYmmdd%%hhmiss%.sql
 del balance.db
 sqlite3 balance.db ".read  balance.sql"
 echo.
